b43e98e57bf4093913600f29b574bf1d1e2e59ec
mothukuru saikiran <obscuresai.com>
2025-01-02 11:21:48 +0530

    message

--- shell.sh
+++ shell.sh
@@ -1,22 +1,15 @@
-#!/bin/bash
-
-SOURCE_DIR=/tmp/shell-logs
+#!/bin/bash 
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+SOURCE_DIR="/tmp/shell-logs"
+
 if [ ! -d $SOURCE_DIR ]
 then 
-    echo -e "$R $SOURCE_DIR does not exists $N"
+    echo -e "$R Source_DIR: $SOURCE_DIR does not exists $N"
 fi 
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +30 -name "*.log")
-
-while IFS= read -r line 
-do
-    echo "Deleting line: $line"
-    rm -rf $line
-done <<< $FILES_TO_DELETE
-
+FILES_TO_DELETE=$(find $SOURCE_DIR -type -d rm -rf)
