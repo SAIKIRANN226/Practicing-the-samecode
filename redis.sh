4c1fcced08e63b95ec43c004fe18cac355a3a508
mothukuru saikiran <obscuresai.com>
2025-02-14 14:21:06 +0530

    shell

--- redis.sh
+++ redis.sh
@@ -27,7 +27,7 @@ else
     echo -e "$Y Script started executing at $DATE $N"
 fi 
 
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
 
 VALIDATE $? "Installing repo file"
 
